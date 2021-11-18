fbd4a00dcf39b61e36634c7267c2075241e43f66
Eric Enouen <erenouen@gmail.com>
2021-11-17 21:31:35 -0500

    Attempt with distance threshold to merge superpixels by color.

--- Color.py
+++ Color.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 from skimage import io, feature
 from skimage.segmentation import slic, mark_boundaries
 import numpy as np
+from skimage.color import rgb2hsv
+import Edge
+from scipy import signal, ndimage
+import cv2 as cv
 
 # Run the superpixel algorithm on a target image
 def detectColors(imgPath):
@@ -9,7 +13,7 @@ def detectColors(imgPath):
     # https://github.com/scikit-image/scikit-image/blob/main/skimage/segmentation/slic_superpixels.py#L110-L384
     img = io.imread(imgPath).astype(float) / 255.
     segments_slic = slic(img,
-                        n_segments = 100, # The number of labels in the output
+                        n_segments = 1000, # The number of labels in the output
                         compactness = .01, # How much weight to give to space proximity
                         max_iter = 100, # Max number of k-means iterations
                         sigma = .5, # Amount of gaussian smoothing
@@ -26,9 +30,35 @@ def detectColors(imgPath):
     fig = plt.figure()
     ax = fig.add_axes([0, 0, 1, 1])
     ax.imshow(mark_boundaries(img, segments_slic))
-    # plt.imshow(img)
     plt.show()
-    # getNcolors(img, listColors, 5)
+    getNcolors(img, listColors, 5)
+    row, col = np.shape(segments_slic)
+    for i in range(row):
+        for j in range(col):
+            img[i][j] = listColors[segments_slic[i][j]]
+
+    plt.imshow(img)
+    plt.show()
+    # Calculate Gaussian masks
+    sigma1 = 1.
+    Gx, Gy = Edge.gaussDeriv2D(sigma1)
+
+    # Apply masks to image to get magnitude of change
+    origImg = io.imread(imgPath, as_gray=True)
+    gxIm = np.float64(ndimage.correlate(origImg, Gx, mode='nearest'))
+    gyIm = np.float64(ndimage.correlate(origImg, Gy, mode='nearest'))
+    magIm = np.hypot(gxIm, gyIm)
+    magIm *= 255.0 / np.max(magIm)
+
+    # Threshold detections
+    tIm = magIm > 25
+    tIm = tIm.astype(np.uint8)
+    contours, hierarchy = cv.findContours(tIm,
+                                cv.RETR_LIST,
+                                cv.CHAIN_APPROX_NONE)
+    cv.drawContours(img, contours, -1, (0,0,0), thickness = 2)
+    plt.imshow(img)
+    plt.show()
     return img
 
 # Set the color of each pixel to the average of its segment
@@ -50,11 +80,20 @@ def matchColors(img, segments_slic):
             img[i][j] = listColors[segments_slic[i][j]]
     return listColors
 
-def getNcolors(img, listColors, segments_slic, n):
+def getNcolors(img, listColors, n):
     # Use connected components ideas applied to color images?
     # Use a flood fill algorithm somehow?
     # Measure the distances between all of the pixels and iteratively merge
     # the closest ones until the select number is acquired?
-    
+    for i in range(len(listColors)):
+        for j in range(len(listColors)):
+            if euclideanColorDist(listColors[i], listColors[j]) < .2:
+                listColors[j] = listColors[i]
+
     # Could probably have a conversation with Dr. Barker about this
-    print()
+
+def euclideanColorDist(color1, color2):
+    return np.sqrt(np.power(color1[0] - color2[0], 2) + 
+           np.power(color1[1] - color2[1], 2) + 
+           np.power(color1[2] - color2[2], 2))
+           
